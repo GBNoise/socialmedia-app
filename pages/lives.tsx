83fcb8a96e86b507b0fcdbb2e92715bcd1804598
GBNoise <gbolanos2002@gmail.com>
2022-10-27 03:07:55 -0600

    lives page

--- pages/lives.tsx
+++ pages/lives.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { LivesContainer } from "../components/lives/LivesContainer";
 import Page from "../components/Page";
-
+import styles from "../styles/lives.module.scss";
 const Lives = () => {
   return (
-    <Page title="Lives" description="Live broadcasts">
-      <h1>Lives</h1>
+    <Page
+      title="Lives"
+      description="Live broadcasts"
+      className={styles.livesContainer}
+    >
+      <LivesContainer />
     </Page>
   );
 };
