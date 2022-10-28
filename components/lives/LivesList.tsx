05cd644e4aab9a5f6e2ebc14880153e97de418a6
GBNoise <gbolanos2002@gmail.com>
2022-10-28 02:30:14 -0600

    added functionality to scroll through lives

--- components/lives/LivesList.tsx
+++ components/lives/LivesList.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
+import React, { useRef } from "react";
 import { LivesListProps } from "../../constants/types";
 import styles from "../../styles/lives.module.scss";
 import { LiveThumbnail } from "./LiveThumbnail";
@@ -7,10 +8,37 @@ export const LivesList: React.FC<LivesListProps> = ({
   title,
   endpoint,
 }): JSX.Element => {
+  const listRef = useRef<HTMLUListElement>(null);
+
+  const handleScroll = (action?: "left" | "right") => {
+    if (!listRef.current) return;
+    let scrollValue = listRef.current.scrollLeft;
+
+    if (action === "left") scrollValue -= 900;
+    if (action === "right") scrollValue += 900;
+
+    listRef.current.scroll({
+      left: scrollValue,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className={styles.livesList}>
       <h1 className={styles.livesListTitle}>{title}</h1>
-      <ul className={styles.livesUl}>
+      <button
+        className={styles.listController}
+        onClick={() => handleScroll("right")}
+      >
+        <ArrowRightIcon />
+      </button>
+      <button
+        className={styles.listController}
+        onClick={() => handleScroll("left")}
+      >
+        <ArrowLeftIcon />
+      </button>
+      <ul className={styles.livesUl} ref={listRef}>
         <li>
           <LiveThumbnail />
         </li>
