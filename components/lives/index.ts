48e05f2bc48b8132fb7083ea739fa086e2557d80
GBNoise <gbolanos2002@gmail.com>
2022-10-30 18:21:48 -0600

    added exports

--- components/lives/index.ts
+++ components/lives/index.ts
@@ -2,3 +2,5 @@ export * from "./LivesList";
 export * from "./LivesContainer";
 export * from "./LivesHeader";
 export * from "./LiveThumbnail";
+export * from "./LivePlayer";
+export * from "./SingleLiveContainer";
